aa0dc315304c523fdf2ee672538a659a0e458cb0
Roman Schmid <bummzack@gmail.com>
2021-05-14 10:46:20 +0200

    Building a stub

---
+++ src/Api/Client.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Mutoco\Mplus\Api;
+
+
+class Client
+{
+
+}
---
+++ src/Task/ImportTask.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Task that runs the import
+ */
+
+namespace Mutoco\Mplus\Task;
+
+
+use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
+
+class ImportTask extends AbstractQueuedJob
+{
+
+}
