e13876a4eda668dbcd347c204446ddb39fd03f6c
Roman Schmid <bummzack@gmail.com>
2021-08-23 17:22:37 +0200

    Update cms fields of the datarecord extension

--- src/Extension/DataRecordExtension.php
+++ src/Extension/DataRecordExtension.php
@@ -3,6 +3,7 @@
 namespace Mutoco\Mplus\Extension;
 
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataExtension;
 
 class DataRecordExtension extends DataExtension
@@ -25,8 +26,20 @@ class DataRecordExtension extends DataExtension
         'MplusID' => '__id'
     ];
 
+    // In case of fluent, don't translate the module field
+    private static $field_exclude = [
+        'Module'
+    ];
+
     public function updateCMSFields(FieldList $fields)
     {
+        $fields->removeByName('MplusID');
+        $fields->addFieldToTab(
+            'Root.Main',
+            TextField::create('MplusID', _t(__CLASS__ . '.MplusID', 'Museum Plus ID')),
+            'Module'
+        );
+        $fields->dataFieldByName('Imported')->setDescription(null);
         $fields->makeFieldReadonly(['Imported', 'MplusID', 'Module']);
     }
 }
